a01138a4baa0590050ecaf595779c2a56ea9b191
Stackbit Code Editor <projects@stackbit.com>
2021-09-08 14:53:28 +0000

    AppConfig.ts: changed

--- src/utils/AppConfig.ts
+++ src/utils/AppConfig.ts
@@ -1,6 +1,6 @@
 export const AppConfig = {
   site_name: 'API',
-  title: 'Nextjs Starter',
+  title: 'Amor',
   description: 'Starter code for your Nextjs Boilerplate with Tailwind CSS',
   locale: 'en',
 };
